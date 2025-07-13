bd2adb27a016d64539d82cc84cd4a5551216cb39
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-07-13 15:34:34 +0530

    Added the correct created At

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -99,7 +99,8 @@ router.post("/login",async (req,res)=>{
                 id : user._id,
                 username : user.username,
                 email : user.email,
-                profileImage : user.profileImage
+                profileImage : user.profileImage,
+                createdAt : user.createdAt
             }
         })
     }catch(e){
